0eaff47f4911420f04d425471e12c64f1ab738f0
Jude Nyahe <judeedemnyahe@gmail.com>
2021-12-23 21:56:46 +0000

    sidenav open and close effect

--- store/static/js/base_script.js
+++ store/static/js/base_script.js
@@ -1,3 +1,22 @@
-function sayHello() {
-    alert("Hello World")
-}
+window.addEventListener("load", () => {
+    document.body.classList.remove("preload");
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const nav = document.querySelector(".nav");
+    const sidenav = document.querySelector(".right-sidebar");
+
+    document.querySelector("#btnNav").addEventListener("click", () => {
+        nav.classList.add("nav--open");
+    });
+
+
+    document.querySelector(".container-wrapper").addEventListener("click", () => {
+        nav.classList.remove("nav--open");
+    });
+    document.querySelector(".cart-close-button").addEventListener("click", () => {
+        nav.classList.remove("nav--open");
+    });
+
+
+});
